5b92c0dd2d7f5d955ad238777acd92203163a114
pluulove <pluulove@gmail.com>
2020-12-06 15:49:38 +0900

    활서화 여부 처리 대응

--- app/src/main/java/com/pluu/lintstudy/SampleAnnotation.kt
+++ app/src/main/java/com/pluu/lintstudy/SampleAnnotation.kt
@@ -2,4 +2,4 @@ package com.pluu.lintstudy
 
 @Retention(AnnotationRetention.BINARY)
 @Target(AnnotationTarget.CLASS)
-annotation class SampleAnnotation
+annotation class SampleAnnotation(val isEnabled: Boolean = true)
--- lint/src/main/java/com/pluu/lint/PropertyWithExcludeFirstCommentDetector.kt
+++ lint/src/main/java/com/pluu/lint/PropertyWithExcludeFirstCommentDetector.kt
@@ -21,12 +21,21 @@ class PropertyWithExcludeFirstCommentDetector : Detector(), SourceCodeScanner {
         return object : UElementHandler() {
 
             override fun visitClass(node: UClass) {
-                if (classPackageName(node) != "com.pluu.lintstudy.exclude_first_comment") return
+                // 테스트 패키지내에서만 동작
+                if (classPackageName(node)?.endsWith("exclude_first_comment") == false) return
+
+                // Target Annotation 체크
+                // 활성화 여부 체크
+                val attribute = node.findAnnotation(TARGET_ANNOTATION)
+                    ?.findAttributeValue(ATTR_VALUE) ?: return
+                val isEnable = ConstantEvaluator.evaluate(context, attribute) as? Boolean ?: false
+
+                if (!isEnable) return
 
                 context.report(ISSUE, context.getNameLocation(node), node.asRecursiveLogString())
-                if (node.hasAnnotation(targetAnnotation)) {
+                if (node.hasAnnotation(TARGET_ANNOTATION)) {
                     val parent = node.getUastParentOfType(UClass::class.java, false)!!
-                    if (parent.hasAnnotation(targetAnnotation)) {
+                    if (parent.hasAnnotation(TARGET_ANNOTATION)) {
                         for (field in parent.fields) {
                             if (isKotlin) {
                                 findFieldInKotlin(field)
@@ -78,7 +87,8 @@ class PropertyWithExcludeFirstCommentDetector : Detector(), SourceCodeScanner {
         (node.containingFile as? PsiJavaFile)?.packageName
 
     companion object {
-        val targetAnnotation = "com.pluu.lintstudy.SampleAnnotation"
+        val TARGET_ANNOTATION = "com.pluu.lintstudy.SampleAnnotation"
+        val ATTR_VALUE = "isEnabled"
 
         val ISSUE = Issue.create(
             id = "SampleDetector",
