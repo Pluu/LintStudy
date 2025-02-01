05ce9d7334daddae64a499bd9912554573d5e1f2
pluu <pluulove@gmail.com>
2025-02-01 10:50:18 +0900

    Update settings.gradle

--- settings.gradle
+++ settings.gradle
@@ -1,11 +1,18 @@
 pluginManagement {
     repositories {
-        google()
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
         mavenCentral()
         gradlePluginPortal()
     }
 }
 dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
