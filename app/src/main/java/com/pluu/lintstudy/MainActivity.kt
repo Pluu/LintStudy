e823a33efdc78bf63231758987239d4775d7fd60
pluu <pluulove@gmail.com>
2025-08-19 23:41:34 +0900

    소스 정리

--- app/src/main/java/com/pluu/lintstudy/MainActivity.kt
+++ app/src/main/java/com/pluu/lintstudy/MainActivity.kt
@@ -4,23 +4,16 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.ui.platform.LocalContext
-import com.pluu.lintstudy.inject.InjectSampleActivity
 import com.pluu.sample.SampleLayout
 import com.pluu.sample.ui.theme.SampleTheme
-import com.pluu.sample.utils.startActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            val context = LocalContext.current
             SampleTheme {
                 SampleLayout(title = "Sample") {
-                    ListItem(title = "Inject") {
-                        context.startActivity<InjectSampleActivity>()
-                    }
                 }
             }
         }
